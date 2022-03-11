3f03a64a6e1270a43098100159ac6f29baf0cc2d
Adrian Miasik <github@adrian-miasik.com>
2022-03-11 18:33:59 -0500

    Create plugin template using StreamDeckToolkit

---
+++ RoshanTimer/RoshanTimer.cmd
@@ -0,0 +1,2 @@
+﻿cd %appdata%\Elgato\StreamDeck\Plugins\com.adrian-miasik.roshan-timer
+RoshanTimer.exe -break %*
---
+++ RoshanTimer/models/CounterSettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace RoshanTimer.Models
+{
+  public class CounterSettingsModel
+  {
+	public int Counter { get; set; } = 0;
+  }
+}
