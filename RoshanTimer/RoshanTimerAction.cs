1e1422e15a6bd1b01a0375b0ddbec64df8d2849f
Adrian Miasik <github@adrian-miasik.com>
2022-03-11 20:25:45 -0500

    Create tick method that invokes once a second

--- RoshanTimer/RoshanTimerAction.cs
+++ RoshanTimer/RoshanTimerAction.cs
@@ -1,13 +1,33 @@
 ﻿using StreamDeckLib;
 using StreamDeckLib.Messages;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace RoshanTimer
 {
     [ActionUuid(Uuid="com.adrian-miasik.roshan-timer.DefaultPluginAction")]
     public class RoshanTimerAction : BaseStreamDeckActionWithSettingsModel<Models.CounterSettingsModel>
     {
-        public override async Task OnKeyDown(StreamDeckEventPayload args)
+        private Timer timer;
+        
+        public override Task OnKeyDown(StreamDeckEventPayload args)
+        {
+            if (timer == null)
+            {
+                timer = new Timer();
+                timer.Elapsed += (sender, eventArgs) =>
+                {
+                    Tick(args);
+                };
+                timer.AutoReset = true;
+                timer.Interval = 1000; // Tick one per second
+                timer.Start();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private async void Tick(StreamDeckEventPayload args)
         {
             SettingsModel.Counter++;
             await Manager.SetImageAsync(args.context, "images/blank.png");
