56165e8b7899d60e41daa3da519d96deb0c49e96
Adrian Miasik <github@adrian-miasik.com>
2022-03-11 23:58:28 -0500

    Code comments and clean up

--- RoshanTimer/RoshanTimerAction.cs
+++ RoshanTimer/RoshanTimerAction.cs
@@ -10,9 +10,10 @@ namespace RoshanTimer
     {
         private Timer timer;
         private bool isTimerPaused;
-        
+
         public override async Task OnKeyUp(StreamDeckEventPayload args)
         {
+            // First press
             if (timer == null)
             {
                 timer = new Timer();
@@ -31,30 +32,59 @@ namespace RoshanTimer
                 return;
             }
             
+            // Play / Pause
             if (isTimerPaused)
             {
-                timer.Start();
-                isTimerPaused = false;
-                await Manager.SetTitleAsync(args.context, GetFormattedString(SettingsModel.Counter));
+                ResumeTimer(args);
             }
-            else if (!isTimerPaused)
+            else
             {
-                // Pause timer
-                timer.Stop();
-                isTimerPaused = true;
-                
-                await Manager.SetTitleAsync(args.context, "Paused");
+                PauseTimer(args);
             }
 
             await base.OnKeyUp(args);
         }
 
+        /// <summary>
+        /// Resumes our timer so it can keep accumulating seconds. Useful for when the Dota 2 match is about to be
+        /// unpaused.
+        /// </summary>
+        /// <param name="args"></param>
+        private async void ResumeTimer(StreamDeckEventPayload args)
+        {
+            // Resume timer
+            timer.Start();
+            isTimerPaused = false;
+            await Manager.SetTitleAsync(args.context, GetFormattedString(SettingsModel.Counter));
+        }
+
+        /// <summary>
+        /// Pauses the timer so it can no longer accumulate seconds. Useful for when the Dota 2 match is paused.
+        /// </summary>
+        /// <param name="args"></param>
+        private async void PauseTimer(StreamDeckEventPayload args)
+        {
+            timer.Stop();
+            isTimerPaused = true;
+                
+            await Manager.SetTitleAsync(args.context, "Paused");
+        }
+
+        /// <summary>
+        /// An update method that gets invoked every second. Intended for timer incrementing.
+        /// </summary>
+        /// <param name="args"></param>
         private async void Tick(StreamDeckEventPayload args)
         {
             SettingsModel.Counter++;
             await Manager.SetTitleAsync(args.context, GetFormattedString(SettingsModel.Counter));
         }
 
+        /// <summary>
+        /// Returns a string in the "0:00" format.
+        /// </summary>
+        /// <param name="totalSeconds"></param>
+        /// <returns></returns>
         private string GetFormattedString(int totalSeconds)
         {
             int totalMinutes = totalSeconds / 60;
