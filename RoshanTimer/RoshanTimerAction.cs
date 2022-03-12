74c0109c15d9c2be2cdf9028ab38a395ea6f813d
Adrian Miasik <github@adrian-miasik.com>
2022-03-11 20:16:45 -0500

    Replace base logic and add blank image

--- RoshanTimer/RoshanTimerAction.cs
+++ RoshanTimer/RoshanTimerAction.cs
@@ -1,32 +1,17 @@
 ﻿using StreamDeckLib;
 using StreamDeckLib.Messages;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RoshanTimer
 {
-  [ActionUuid(Uuid="com.adrian-miasik.roshan-timer.DefaultPluginAction")]
-  public class RoshanTimerAction : BaseStreamDeckActionWithSettingsModel<Models.CounterSettingsModel>
-  {
-	public override async Task OnKeyUp(StreamDeckEventPayload args)
-	{
-	  SettingsModel.Counter++;
-	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
-	}
-
-	public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
-	{
-	  await base.OnDidReceiveSettings(args);
-	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
-	}
-
-	public override async Task OnWillAppear(StreamDeckEventPayload args)
-	{
-	  await base.OnWillAppear(args);
-	  await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
-	}
-
-  }
+    [ActionUuid(Uuid="com.adrian-miasik.roshan-timer.DefaultPluginAction")]
+    public class RoshanTimerAction : BaseStreamDeckActionWithSettingsModel<Models.CounterSettingsModel>
+    {
+        public override async Task OnKeyDown(StreamDeckEventPayload args)
+        {
+            SettingsModel.Counter++;
+            await Manager.SetImageAsync(args.context, "images/blank.png");
+            await Manager.SetTitleAsync(args.context, SettingsModel.Counter.ToString());
+        }
+    }
 }
